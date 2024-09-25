39cb754850901208b62c5c985d0624eff6135af1
abhay333d <abhaydandge333@gmail.com>
2024-09-25 15:21:37 +0530

    added the water color

--- src/shaders/terrain/fragment.glsl
+++ src/shaders/terrain/fragment.glsl
@@ -1,10 +1,10 @@
 
 uniform vec3 uColorWaterDeep;
-uniform vec3 colorWaterSurface;
-uniform vec3 colorSand;
-uniform vec3 colorGrass;
-uniform vec3 colorSnow;
-uniform vec3 colorRock;
+uniform vec3 uColorWaterSurface;
+uniform vec3 uColorSand;
+uniform vec3 uColorGrass;
+uniform vec3 uColorSnow;
+uniform vec3 uColorRock;
 
 varying vec3 vPosition;
 
@@ -12,6 +12,10 @@ varying vec3 vPosition;
 void main(){
     //color 
     vec3 color = vec3(1.0);
+
+    //Water
+    float waterSurfaceMix = smoothstep(-1.0, -0.1, vPosition.y);
+    color = mix(uColorWaterDeep, uColorWaterSurface, waterSurfaceMix);
 
     //Final color
     csm_DiffuseColor = vec4(color, 1.0);
