a00a2d6d46f4ec2d4999cea33c19fde8305ae1bb
ojasvi004 <ojasvidoye@gmail.com>
2024-10-13 23:35:11 +0530

    fixed the path

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { RGBELoader } from "three/examples/jsm/Addons.js";
-import { Evaluator } from "three-bvh-csg";
-import { Brush } from "three-bvh-csg";
-import { SUBTRACTION } from "three-bvh-csg";
+import { Evaluator, Brush, SUBTRACTION } from "../node_modules/three-bvh-csg";
 import CustomShaderMaterial from "three-custom-shader-material/vanilla";
 import GUI from "lil-gui";
 import terrainVertexShader from "./shaders/terrain/vertex.glsl";
